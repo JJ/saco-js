ec68d75f4aab065264ec2b05e6cbe35783de0e3d
JJ Merelo <jjmerelo@gmail.com>
2023-03-15 17:03:16 +0100

    :sparkles: :memo: #3

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Value or 0, returns the value corresponding to a key if it exists, 0 if it does not.
+ *
+ * @param {Object} hash - a hash, associative array or Object
+ * @param {String} key  - A string representing a key
+ * @returns value corresponding to the `key`or 0 if it does not exist
+ */
 export function valOr0(hash, key) {
   return key in hash ? hash[key] : 0;
 }
