93452a955d5e982470084f7470df2b459b31fd7c
JJ Merelo <jjmerelo@gmail.com>
2023-03-15 18:12:20 +0100

    Writing tests

--- test/deno/basic_test.js
+++ test/deno/basic_test.js
@@ -1,12 +1,18 @@
 import { assertEquals } from "https://deno.land/std@0.174.0/testing/asserts.ts";
 
-import { valOr0 } from "../../index.js";
+import { valOr0, incrementOrInit } from "../../index.js";
 
-const dummy = { foo: 3, bar: 2 };
+const FOO = 3;
+let dummy = { foo: FOO, bar: 2 };
 
 Deno.test(function valOr0Test() {
   for (const i in dummy) {
     assertEquals(valOr0(dummy, i), dummy[i]);
   }
   assertEquals(valOr0(dummy, "thisDoesNotExist"), 0);
+});
+
+Deno.test(function incrementOrInitTest() {
+  incrementOrInit(dummy, foo);
+  assertEquals(dummy["foo"], FOO + 1);
 });
