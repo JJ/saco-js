7a2dd1455b06c408ae3afd78cf9d93cb57dbba9c
JJ Merelo <jjmerelo@gmail.com>
2023-03-15 18:06:21 +0100

    :sparkles: another auxiliary function

--- index.js
+++ index.js
@@ -12,3 +12,26 @@
 export function valOr0(hash, key) {
   return key in hash ? hash[key] : 0;
 }
+
+/**
+ * Initializes a hash value for a key if it does not exist, increments if it does
+ * @param {*} hash - hash to be mutated
+ * @param {*} key  - key whose value is going to be set or changed
+ */
+export function incrementOrInit(hash, key) {
+  if (key in hash) {
+    hash[key]++;
+  } else {
+    hash[key] = 1;
+  }
+}
+
+export function hashify(anArray) {
+  return anArray.reduce((runningHash, el) => {
+    if (el in runningHash) {
+      runningHash[el]++;
+    } else {
+      runningHash[el] = 1;
+    }
+  });
+}
