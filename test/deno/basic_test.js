e9f667625f64a5142344b8d8cf9a7670f4a77a63
JJ Merelo <jjmerelo@gmail.com>
2023-03-15 12:36:25 +0100

    Test for Deno exclusively, closes #5

---
+++ test/deno/basic_test.js
@@ -0,0 +1,12 @@
+import { assertEquals } from "https://deno.land/std@0.174.0/testing/asserts.ts";
+
+import { valOr0 } from "../../index.js";
+
+const dummy = { foo: 3, bar: 2 };
+
+Deno.test(async function valOr0Test() {
+  for (let i in dummy) {
+    assertEquals(valOr0(dummy, i), dummy[i]);
+  }
+  assertEquals(valOr0(dummy, "thisDoesNotExist"), 0);
+});
